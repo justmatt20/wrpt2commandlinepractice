213c2a252738e99c1592a47251c6ac43186eaa97
Justine Matthews <justinematt14@gmail.com>
2021-06-14 22:36:59 -0400

    everything but previous returnting to 0

--- unit3/user-directory/src/App.js
+++ unit3/user-directory/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Buttons from './components/buttons/Buttons'
+import data from '../src/Data'
 import People from './components/people/People'
 import Header from './components/header/Header'
 import '../src/App.css'
@@ -9,14 +9,13 @@ class App extends React.Component {
 
 render() {
       return (
-        <div>
+        
             
      <div>
        <Header />
-       <People />
-        {/* <Buttons /> */}
+       <People  peopleData={data}/>
         </div>
-        </div>
+        
       )}}
 
          
--- unit3/user-directory/src/components/people/People.js
+++ unit3/user-directory/src/components/people/People.js
@@ -1,78 +1,69 @@
-import React, { Component } from 'react'
-import data from '../../Data'
-import './people.css'
+import React, { useState } from "react";
+import data from "../../Data";
+import "./people.css";
+
+const People = ({ peopleData }) => {
+  const [current, setCurrent] = useState(0);
+  const length = peopleData.length;
 
 
-class People extends Component {
-    constructor() {
-        super()
-            this.state = {
-                people: []
-            }
-        
-    }
+  const nextPerson = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  console.log(current);
+  const prevPerson = () => {
+    setCurrent(current === length - 1 ? 0 : current - 1);
+  };
 
-    getNext = (i) => {
-        for (let i = 0; i <= data.length; i++) {
-           return i
-            }
-        
-                
-            }
-    
-    
-    getPrev = () => {
-        for (let i=0; i<=data.length; i--) {
-            return data[i]
-        }
-    
-    }
-
-
-render () {
-    // console.log(data)
-    
-    const mappedPeople = data.map((info) => (
-       
-                    <div className="info-box" key={info.id}>
-                        <div className="index">
-                            <div>{info.id}/25</div>
-                        </div>
-                        <div className="person">
-                        <div className="names">
-                            <div className="style">{info.name.first} {info.name.last}</div>
-                            </div>
-                            <div className="from">
-                        <p className="info">From:</p> <p className="style">{info.city}, {info.country}</p>
-                        </div>
-                        <div className="title">
-                        <p className="info">Job Title:</p> <p className="style">{info.title}</p>
-                        </div>
-                        <div className="job">
-                        <p className="info">Employer:</p> <p className="style">{info.employer}</p>
-                        </div>
-                        <div className="movies">Favorite Movies:</div>
-                        <ol className="list">
-                            <li>{info.favoriteMovies[0]}</li>
-                            <li>{info.favoriteMovies[1]}</li>
-                            <li>{info.favoriteMovies[2]}</li>
-                        </ol>
-                        </div>
-                    </div>
-    ));
+  
+  return (
+    <>
+      {data.map((info, index) => {
         return (
-            <div className="body">
-        
-            <div>{mappedPeople[0]}</div>  
-           
+            <div className={index === current ? 'slide active' : info} key={index}>
+                {index === current && ( <div className="body">
+            <div className="info-box" key={info.id}>
+              <div className="index">
+                <div>{info.id}/25</div>
+              </div>
+              <div className="person">
+                <div className="names">
+                  <div className="style">
+                    {info.name.first} {info.name.last}
+                  </div>
+                </div>
+                <div className="from">
+                  <p className="info">From:</p>{" "}
+                  <p className="style">
+                    {info.city}, {info.country}
+                  </p>
+                </div>
+                <div className="title">
+                  <p className="info">Job Title:</p>{" "}
+                  <p className="style">{info.title}</p>
+                </div>
+                <div className="job">
+                  <p className="info">Employer:</p>{" "}
+                  <p className="style">{info.employer}</p>
+                </div>
+                <div className="movies">Favorite Movies:</div>
+                <ol className="list">
+                  <li>{info.favoriteMovies[0]}</li>
+                  <li>{info.favoriteMovies[1]}</li>
+                  <li>{info.favoriteMovies[2]}</li>
+                </ol>
+              </div>
+            </div>
             <div className="buttons">
-                <button  onClick={this.getPrev}> &#60; Previous</button>
-               <button  onClick={this.getNext}>Next  &#62; </button>
+              <button className="previous"onClick={prevPerson}> &#60; Previous</button>
+              <button className="next"onClick={nextPerson}>Next &#62; </button>
             </div>
-            </div>
-            
-        )
-    
-    
-}}
+            </div>)}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
 export default People;
