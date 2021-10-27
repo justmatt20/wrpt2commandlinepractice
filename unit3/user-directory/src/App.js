758ffec5af765d1618dee48d6aa87e9d0c2b9b66
Justine Matthews <justinematt14@gmail.com>
2021-10-26 21:03:10 -0400

    git issues

--- unit3/user-directory/src/App.js
+++ unit3/user-directory/src/App.js
@@ -1,29 +1,18 @@
-import React from 'react'
-import data from '../src/Data'
-import People from './components/people/People'
-import Header from './components/header/Header'
-import '../src/App.css'
-
+import React from "react";
+import data from "../src/Data";
+import People from "./components/people/People";
+import Header from "./components/header/Header";
+import "../src/App.css";
 
 class App extends React.Component {
-
-render() {
-      return (
-        
-            
-     <div>
-       <Header />
-       <People  peopleData={data}/>
-        </div>
-        
-      )}}
-
-         
-  
-  
-  
-        
-        
-
+  render() {
+    return (
+      <div>
+        <Header />
+        <People peopleData={data} />
+      </div>
+    );
+  }
+}
 
 export default App;
